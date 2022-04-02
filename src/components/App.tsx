2195b356f1c7b2b04608b8272e3cf51b872ff68a
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-04-02 16:43:55 +0900

    appファイルの中で実装完了

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { ChangeEvent, FC, useState } from "react";
+import styled from "styled-components";
 
-const App = () => {
-  return <div>App</div>;
+export const App: FC = () => {
+  const [text, setText] = useState<string>("");
+
+  const [memos, setMemos] = useState<string[]>([]);
+
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
+    setText(e.target.value);
+
+  const onClickAdd = () => {
+    const newMemos = [...memos];
+    newMemos.push(text);
+    setMemos(newMemos);
+    setText("");
+  };
+
+  const onClickDelete = (index: number) => {
+    const newMemos = [...memos];
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+  };
+
+  return (
+    <div>
+      <h1>簡単メモアプリ</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>追加</SButton>
+      <SContainer>
+        <p>メモ一覧</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <SMemoWrapper>
+                <p>{memo}</p>
+                <SButton onClick={() => onClickDelete(index)}>削除</SButton>
+              </SMemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
+    </div>
+  );
 };
 
-export default App;
+const SButton = styled.button`
+  margin-left: 16px;
+`;
+
+const SContainer = styled.div`
+  border: solid 1px #ccc;
+  padding:16px
+  margin : 8px
+`;
+
+const SMemoWrapper = styled.div`
+display:flex
+align-items:center
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { App } from "./components/App";
 
-ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <React.StrictMode></React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
